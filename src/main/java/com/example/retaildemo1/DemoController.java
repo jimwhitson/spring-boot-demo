d5e2ab016e9e0cab09e9646104f3a8a379bad575
Jim Whitson <jim.whitson@couchbase.com>
2018-08-07 12:47:44 +0100

    closer to final queries

--- src/main/java/com/example/retaildemo1/DemoController.java
+++ src/main/java/com/example/retaildemo1/DemoController.java
@@ -20,7 +20,7 @@ public class DemoController {
         bucket = cluster.openBucket("pricing3");
     }
 
-    @RequestMapping("/latest/{product_id}/{store_id}")
+    @RequestMapping("/latestAtStore/{product_id}/{store_id}")
     public String storeProductLookup(@PathVariable Integer product_id, @PathVariable Integer store_id) {
         N1qlQueryResult result = bucket.query(
                 N1qlQuery.parameterized(
@@ -33,6 +33,26 @@ public class DemoController {
                         "AND store_pricing.product_id = $1 " +
                         "AND store_pricing.store_id = $2;", JsonArray.from(product_id, store_id))
         );
+        return result.toString();
+    }
+
+    @RequestMapping("/latestAtStoreWithGroup/{product_id}/{store_id}")
+    public String storeAndGroupLatestPrice(@PathVariable Integer product_id, @PathVariable Integer store_id) {
+        N1qlQueryResult result = bucket.query(
+                N1qlQuery.parameterized("SELECT store_pricing.store_id, store_pricing.product_id, store_pricing.store_group_id,\n" +
+                                "       store_pricing.store_prices[0] as latest_store_prices,\n" +
+                                "       group_pricing.store_group_prices[0] as latest_group_prices\n" +
+                                "\n" +
+                                "FROM pricing3 AS store_pricing\n" +
+                                " JOIN pricing3 AS group_pricing\n" +
+                                " ON store_pricing.store_group_id = group_pricing.store_group_id\n" +
+                                "\n" +
+                                "   WHERE store_pricing.type = \"store_product\"\n" +
+                                "   AND group_pricing.type=\"store_group_product\"\n" +
+                                "   AND store_pricing.store_id = $1\n" +
+                                "   AND group_pricing.product_id = $2\n" +
+                                "   AND store_pricing.product_id = $2",
+                        JsonArray.from(store_id, product_id)));
         return result.toString();
     }
 }
