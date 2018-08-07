658200ffdeda5db2f2484556476edbcb686eb8a4
Jim Whitson <jim.whitson@couchbase.com>
2018-08-07 23:05:04 +0100

    Just one query, using the N1QL DSL.

--- src/main/java/com/example/retaildemo1/DemoController.java
+++ src/main/java/com/example/retaildemo1/DemoController.java
@@ -9,50 +9,42 @@ import com.couchbase.client.java.document.*;
 import com.couchbase.client.java.document.json.*;
 import com.couchbase.client.java.query.*;
 
+import static com.couchbase.client.java.query.Select.select;
+import static com.couchbase.client.java.query.dsl.Expression.x;
+import static com.couchbase.client.java.query.dsl.Expression.s;
+import static com.couchbase.client.java.query.dsl.Expression.i;
+
+
+
+
 @RestController
 public class DemoController {
     static private Cluster cluster;
     static private Bucket bucket;
 
     static {
-        cluster = CouchbaseCluster.create("couchbase://34.255.147.194");
+        cluster = CouchbaseCluster.create("couchbase://<Server Address>");
         cluster.authenticate("Administrator", "password");
-        bucket = cluster.openBucket("pricing3");
+        bucket = cluster.openBucket("pricing");
     }
 
     @RequestMapping("/latestAtStore/{product_id}/{store_id}")
     public String storeProductLookup(@PathVariable Integer product_id, @PathVariable Integer store_id) {
-        N1qlQueryResult result = bucket.query(
-                N1qlQuery.parameterized(
-                        "select store_pricing.store_id, " +
+        Statement stmt =
+            select("store_pricing.store_id, "+
                         "store_pricing.product_id, " +
                         "store_pricing.store_group_id, " +
-                        "store_pricing.store_prices[0] as latest_store_price " +
-                        "FROM pricing3 as store_pricing " +
-                        "WHERE store_pricing.type=\"store_product\" " +
-                        "AND store_pricing.product_id = $1 " +
-                        "AND store_pricing.store_id = $2;", JsonArray.from(product_id, store_id))
+                        "store_pricing.store_prices[0] as latest_store_price")
+                .from(i("pricing3")).as("store_pricing")
+                .where(x("store_pricing.type").eq(s("store_product"))
+                        .and(x("store_pricing.product_id").eq(x("$prod_id")))
+                        .and(x("store_pricing.store_id").eq(x("$store_id")))
         );
-        return result.toString();
-    }
-
-    @RequestMapping("/latestAtStoreWithGroup/{product_id}/{store_id}")
-    public String storeAndGroupLatestPrice(@PathVariable Integer product_id, @PathVariable Integer store_id) {
-        N1qlQueryResult result = bucket.query(
-                N1qlQuery.parameterized("SELECT store_pricing.store_id, store_pricing.product_id, store_pricing.store_group_id,\n" +
-                                "       store_pricing.store_prices[0] as latest_store_prices,\n" +
-                                "       group_pricing.store_group_prices[0] as latest_group_prices\n" +
-                                "\n" +
-                                "FROM pricing3 AS store_pricing\n" +
-                                " JOIN pricing3 AS group_pricing\n" +
-                                " ON store_pricing.store_group_id = group_pricing.store_group_id\n" +
-                                "\n" +
-                                "   WHERE store_pricing.type = \"store_product\"\n" +
-                                "   AND group_pricing.type=\"store_group_product\"\n" +
-                                "   AND store_pricing.store_id = $1\n" +
-                                "   AND group_pricing.product_id = $2\n" +
-                                "   AND store_pricing.product_id = $2",
-                        JsonArray.from(store_id, product_id)));
-        return result.toString();
+        JsonObject venv = JsonObject.create()
+                .put("prod_id", product_id)
+                .put("store_id", store_id);
+        N1qlQuery query = N1qlQuery.parameterized(stmt, venv);
+        N1qlQueryResult result = bucket.query(query);
+        return result.allRows().toString();
     }
 }
